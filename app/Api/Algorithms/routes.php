f81b0ca560fafbf0b11b2d8077c96e191a02e314
oss6 <ossedb@gmail.com>
2018-03-19 14:23:34 +0000

    Fix

--- app/Api/Algorithms/routes.php
+++ app/Api/Algorithms/routes.php
@@ -2,7 +2,6 @@
 
 Route::get('/regex2nfa/{regex}', 'AlgorithmController@regexToNfa');
 Route::post('/nfa2dfa', 'AlgorithmController@nfaToDfa');
-Route::get('/nfa2dfa/breakpoints', 'AlgorithmController@nfaToDfaBreakpoints');
 Route::post('/minimize-dfa', 'AlgorithmController@minimizeDfa');
 Route::post('/cek-machine/next-step', 'AlgorithmController@cekMachineNextStep');
 Route::post('/cek-machine/run', 'AlgorithmController@cekMachineRun');
